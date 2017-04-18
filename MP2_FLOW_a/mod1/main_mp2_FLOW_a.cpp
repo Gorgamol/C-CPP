196663fa38c8471bfdc6857b4985e4126e021997
Gorgamol <patrick.katthose@googlemail.com>
2017-04-18 10:50:43 +0200

    18.04.2017

---
+++ MP2_FLOW_a/mod1/main_mp2_FLOW_a.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>  /* file main_mp2_FLOW_a.cpp */
+#include <string>
+#include "AnsiConsole.h"
+
+AnsiConsole console; int firstLine; int currentTick; Colors currentColor;
+#define INITPRINT(label) {firstLine=__LINE__;console.printText(2*currentTick,0,label,Colors::BLACK);}
+#define PRINT printLineNumber(__LINE__)
+void printLineNumber(int lineNumber);
+
+void func1(int x);
+void print() {
+  PRINT; func1(6);
+PRINT;}
+void func1(int x) {
+  for(int i = 0; i < x; ++i) {
+    PRINT;
+  } for(int i = 0; i <= x; ++i) {
+    PRINT;
+  }
+}
+
+
+int main(int argc, const char * argv[]) {
+  console.clearScreen();
+  currentColor = Colors::GREEN;
+  print();
+
+  //start_goto();
+  //start_switch();
+  //start_interrupt();
+  //start_coroutine();
+  //start_exception();
+  std::string s;
+  std::cin >> s;
+  return 0;
+}
+
+void printLineNumber(int lineNumber){
+  std::string line = std::to_string(lineNumber);
+  console.printText(currentTick*2, 1+lineNumber-firstLine, line, currentColor);
+  currentTick++;
+}
